1c411b8e70c5000924af7828ccda11ebf8bbd384
wingsiu <wingsiu48@gmail.com>
2025-08-04 16:54:15 +0800

    Add Chi Char and Emoji Support Fixed bug getdefaultstyle

--- Sources/SwiftMath/MathRender/MTTypesetter.swift
+++ Sources/SwiftMath/MathRender/MTTypesetter.swift
@@ -161,10 +161,13 @@ func getDefaultStyle(_ ch:Character) -> UTF32Char {
         // . is treated as a number in our code, but it doesn't change fonts.
         return ch.utf32Char
     } else {
-        NSException(name: NSExceptionName("IllegalCharacter"), reason: "Unknown character \(ch) for default style.").raise()
+        //By Alpha
+        //NSException(name: NSExceptionName("IllegalCharacter"), reason: "Unknown character \(ch) for default style.").raise()
+        //By Alpha
     }
     return ch.utf32Char
 }
+
 
 // mathcal/mathscr (caligraphic or script)
 func getCaligraphic(_ ch:Character) -> UTF32Char {
