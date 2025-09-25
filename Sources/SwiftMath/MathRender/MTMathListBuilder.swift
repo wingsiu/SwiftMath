4e78e7ef4ecde17b2b3e6ee67d7c2b8842069fc9
wingsiu <wingsiu48@gmail.com>
2025-09-25 14:03:02 +0800

    change nucleus to plain string when converting latex

--- Sources/SwiftMath/MathRender/MTMathListBuilder.swift
+++ Sources/SwiftMath/MathRender/MTMathListBuilder.swift
@@ -490,7 +490,7 @@ public struct MTMathListBuilder {
                     if let command = MTMathAtomFactory.latexSymbolName(for: atom) {
                         str += "\\\(command) "
                     } else {
-                        str += "\(atom.nucleus)"
+                        str += "\(String(atom.nucleus))"
                     }
                 }
                 
